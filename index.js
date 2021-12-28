1aeccb5711987e2f84d684f6fda0e4dc4cadbcd2
Emiliano <emilianogorgellon49@gmail.com>
2021-12-28 18:16:10 -0300

    Fetch

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let express = require("express");
 let app = express();
 let cors = require("cors");
-let path = require("path");
 let Socket = require("./utils/sockets")
 const PORT = 3000;
 let {Server: HttpServer} = require("http");
+let path = require("path");
 
 // Middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors("*"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // handlebars
 app.set("views", path.join(__dirname , "views", "ejs"));
